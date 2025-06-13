affb74c3968ae82561ff671deb291ea32c05fe8f
Alexander Koychev <durotar15@gmail.com>
2025-06-13 15:23:50 +0300

    Add files via upload

---
+++ KursovProekt (1)/KursovProekt/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KursovProekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = new string(Class1.M);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            groupBox2.Text = "Текст";
+            button1.Text = "Криптирай";
+            textBox1.Text = new string(Class1.M);
+            textBox2.Text = Class1.TestText;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text == string.Empty)
+                {
+                    MessageBox.Show("Въведи текст преди да започнеш процеса на криптиране/декриптиране.", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textBox2.Focus();
+                    return;
+                }
+
+
+                if (radioButton1.Checked)
+                {
+                    textBox3.Text = Class1.Encrypt(textBox2.Text).Item2;
+                }
+                else
+                {
+                    textBox3.Text = Class1.Decrypt(textBox2.Text);
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message, "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Text = "Декриптирай";
+            groupBox2.Text = "Криптограма";
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ KursovProekt (1)/KursovProekt/Директно Заместване.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursovProekt
+{
+    static class Class1
+    {
+        public static char[] M { get; set; } = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЮЯ0123456789 ".ToCharArray();
+        public static Dictionary<char, int> SubsCodes { get; private set; }
+        public const string TestText = "МРАЗЯ ДА ПРАВЯ ПРЕЗЕНТАЦИИ";
+        private static void InitializeDictionary()
+        {
+            SubsCodes = new Dictionary<char, int>(M.Length);
+            for (int i = 0; i < M.Length; i++)
+                SubsCodes[M[i]] = i + 1;
+        }
+
+        public static Tuple<string, string> Encrypt(string Text = TestText)
+        {
+            InitializeDictionary();
+            var TextAsChars = Text.ToCharArray();
+            var resultWithEncryptedSpaces = new string[Text.Length];
+            var resultWithoutEncryptedSpaces = new string[Text.Length];
+
+            for (int i = 0; i < TextAsChars.Length; i++)
+            {
+
+                if (!M.Contains(TextAsChars[i])) throw new ArgumentOutOfRangeException(TextAsChars[i].ToString(), $"\nТози текст съдържа символа '{TextAsChars[i]}', който не е част от множеството допустими символи.");
+
+                resultWithEncryptedSpaces[i] = SubsCodes[TextAsChars[i]].ToString();
+                resultWithoutEncryptedSpaces[i] = SubsCodes[TextAsChars[i]].ToString();
+
+                if (Text[i] == ' ')
+                    resultWithoutEncryptedSpaces[i] = "  ";
+
+            }
+
+            return new Tuple<string, string>(String.Join(" ", resultWithoutEncryptedSpaces), String.Join(" ", resultWithEncryptedSpaces));
+        }
+
+        public static string Decrypt(string cryptogram, bool areSpacesEncrypted = true)
+        {
+            InitializeDictionary();
+
+            string[] input;
+
+            if (areSpacesEncrypted)
+            {
+                input = cryptogram.Split(' ');
+                char[] result = new char[input.Length];
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    var isNum = int.TryParse(input[i], out int num);
+                    if (!isNum)
+                        throw new ArgumentException("Пиши само числа!!");
+
+                    result[i] = SubsCodes.SingleOrDefault(x => x.Value == num).Key;
+
+                    if (result[i] == 0)
+                        throw new ArgumentException("Тази криптограма превишава лимита на допустими множества!!");
+                }
+
+                return new String(result);
+            }
+            else
+            {
+                input = cryptogram.Split(new string[] { "    " }, StringSplitOptions.None);
+                List<char> output = new List<char>();
+
+                for (int word = 0; word < input.Length; word++)
+                {
+                    var wordSymbols = input[word].Split(' ');
+                    for (int symbol = 0; symbol < wordSymbols.Length; symbol++)
+                    {
+                        output.Add(SubsCodes.Single(x => x.Value == int.Parse(wordSymbols[symbol])).Key);
+                    }
+
+                    if (word != input.Length - 1) output.Add(' ');
+                }
+
+                return new string(output.ToArray());
+            }
+
+        }
+    }
+}
